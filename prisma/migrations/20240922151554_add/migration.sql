dac6c13f97e18dfd818a0c34da81e60855dee211
wess <wesley.santos@mblabs.com.br>
2024-11-04 18:47:53 -0300

    feat: change db

--- prisma/migrations/20240922151554_add/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "aboutme" TEXT;
-ALTER TABLE "users" ADD COLUMN "cell" TEXT;
-ALTER TABLE "users" ADD COLUMN "facebook" TEXT;
-ALTER TABLE "users" ADD COLUMN "instagram" TEXT;
-ALTER TABLE "users" ADD COLUMN "linkedin" TEXT;
-ALTER TABLE "users" ADD COLUMN "tell" TEXT;
-ALTER TABLE "users" ADD COLUMN "whatsapp" TEXT;
